f69667b187c111a075521473458bdd3c796e38d7
dilmurodov <tolibdilmurodov98@gmail.com>
2022-11-24 01:33:14 +0500

    add ListenToWs and ListenForWs

--- internal/handlers/ws.go
+++ internal/handlers/ws.go
@@ -19,6 +19,20 @@ var UpgradeConnection = websocket.Upgrader{
 	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
+var wsChann = make(chan wsJsonPayload)
+var clients = make(map[*WebSocketConnection]string)
+
+type wsJsonPayload struct {
+	Message string               `json:"message"`
+	Action  string               `json:"action"`
+	Conn    *WebSocketConnection `json:"-"`
+}
+
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
+// Upgrade http connection with socket
 func WsUpgrade(w http.ResponseWriter, r *http.Request) {
 
 	ws, err := UpgradeConnection.Upgrade(w, r, nil)
@@ -29,5 +43,56 @@ func WsUpgrade(w http.ResponseWriter, r *http.Request) {
 	jsonRes := wsJsonResponse{}
 	jsonRes.Message = `<div>Connection Successfully!</div>`
 
+	// create socket connection
+	conn := &WebSocketConnection{Conn: ws}
+
+	// create client
+	clients[conn] = ""
+
+	// send msg to all client about created to connection
 	ws.WriteJSON(jsonRes)
+
+	go ListenForWs(conn)
+}
+
+// ListenForWs is a goroutine that handles communication between server and client, and
+// feeds data into the wsChan
+func ListenForWs(conn *WebSocketConnection) {
+
+	var payload wsJsonPayload
+
+	for {
+		err := conn.Conn.ReadJSON(&payload)
+		if err != nil {
+			// not send
+		} else {
+			payload.Conn = conn
+			wsChann <- payload
+		}
+	}
+}
+
+// ListenToWsChannel is a goroutine that waits for an entry on the wsChan, and handles it according to the
+// specified action
+func ListenToWs(conn *WebSocketConnection) {
+
+	var response wsJsonResponse
+
+	for {
+		e := <-wsChann
+		response.Message = "Some message with action " + e.Action
+	}
+}
+
+// broadcastToAll sends ws response to all connected clients
+func BroadcastToAll(resp *wsJsonResponse) {
+	for client := range clients {
+		err := client.Conn.WriteJSON(resp)
+		if err != nil {
+			
+			logger.Error("---Error-> BroadcastToAll--->", err.Error())
+			_ = client.Conn.Close()
+			delete(clients, client)
+		}
+	}
 }
